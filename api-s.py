e07ffdc1c52c8efa155188e05e90398d45263ea1
Gabriel Vieira <112446183+devgabrielvieira@users.noreply.github.com>
2023-04-02 17:26:02 -0300

    Convertendo a coluna 'data' para datetime
    e filtrando dados de 01/01/2023 para frente.

--- api-s.py
+++ api-s.py
@@ -2,4 +2,11 @@ import pandas as pd
 
 url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=json"
 df = pd.read_json(url)
+
+# Converter a coluna 'data' para o tipo datetime
+df['data'] = pd.to_datetime(df['data'], dayfirst=True)
+
+# Filtrar as linhas com datas a partir de 01/01/2023
+df = df.loc[df['data']>= '2023-01-01']
+
 print(df.head())
