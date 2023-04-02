0ec9f3ccbadcc584777c7722cfb5cfb081d58606
Gabriel Vieira <112446183+devgabrielvieira@users.noreply.github.com>
2023-04-02 19:00:45 -0300

    Importado a função display para melhor formatação
    dos dados.

--- api-s.py
+++ api-s.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from IPython.display import display as dp
 
 url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=json"
 df = pd.read_json(url)
@@ -7,6 +8,6 @@ df = pd.read_json(url)
 df['data'] = pd.to_datetime(df['data'], dayfirst=True)
 
 # Filtrar as linhas com datas a partir de 01/01/2023
-df = df.loc[df['data']>= '2023-01-01']
+df = df.loc[df['data']>= '2023-03-01']
 
-print(df.head())
+dp(df.head())
